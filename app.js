f557bb03539c2847c81feb3482277cbcacf83eb3
ch-sakshi <ch.sakshi005@gmail.com>
2026-07-07 22:21:29 +0530

    add: app.js

---
+++ app.js
@@ -0,0 +1,63 @@
+// Use LOgical Operators to find the whether the age of a person lies between 10 and 20.
+// Ans-
+// let age = 15;
+
+// if (age >= 10 && age <= 20) {
+//   console.log("The age lies between 10 and 20.");
+// } else {
+//   console.log("The age does not lies between 10 and 20.");
+// }
+
+// Q2 - Demonstrate the use of switch case statement in js.
+// Ans-
+
+// let day = 4;
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+
+//   case 4:
+//     console.log("Thursday");
+//     break;
+
+//   case 5:
+//     console.log("Friday");
+//     break;
+
+//   default:
+//     console.log("Invalid Day");
+// }
+
+//Q3- Write a Js program to find whether a number is divisible by 2 and 3.
+// Ans-
+// let num = 12;
+// if (num % 2 == 0 && num % 3 == 0) {
+//   console.log("The number is divisible by 2 and 3");
+// } else {
+//   console.log("The number is not divisible by 2 and 3");
+// }
+
+// Q4- Write a Js program to find whether a number is divisible by either 2 and 3.
+// Ans-
+
+// let num = 18;
+// if (num % 2 == 0 || num % 3 == 0) {
+//   console.log("The number is divisible by 2 either 3");
+// } else {
+//   console.log("The number is not divisible by 2 either 3");
+// }
+
+//Q5- Print "you can drive" or "you cannot drive" based on age being greater than 18 using ternary operator.
+// Ans-
+// let age = 20;
+// let result = age > 18 ? "You can Drive" : "You canot Drive";
+// console.log(result);
